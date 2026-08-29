4946167b9f420a980385d1c5b31f3b701068d622
Shreesh Singh <shreeshsingh1508@gmail.com>
2026-08-29 19:47:40 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,7 @@
+num1 = float(input("enter first number = ")
+num2 = float(input("enter second number = ")
 
+print("addition = ", num1 + num2)
+print("substraction = ", num1 - num2)
+print("mutiplication = ", num1 * num2)
+print("division = ", num1 / num2)
